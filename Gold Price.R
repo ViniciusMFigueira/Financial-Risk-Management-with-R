421f0ba31f0f56fdb1ebe75c6b9c0a1589ebdc0d
ViniciusMFigueira <86022828+ViniciusMFigueira@users.noreply.github.com>
2021-10-07 01:51:35 -0300

    Add files via upload

---
+++ Gold Price.R
@@ -0,0 +1,21 @@
+##Retrieve Gold Prices from FRED
+library(quantmod)
+
+# Retrieve the following data from FRED: naming object as "gold price"
+gold_prices <- getSymbols("GOLDPMGBD228NLBM" ,src="FRED",auto.assign=FALSE)
+
+# Remove all "NA" observations from this series.
+gold_prices <- na.omit(gold_prices)
+
+# Restrict the dates of the series from 20 last years 2001-10-01 to 2021-10-01
+gold_prices <- gold_prices["2001-10-01/2021-10-01"]
+
+#name for gold price list
+names(gold_prices) <- "gold prices"
+
+head(gold_prices,3)
+tail(gold_prices,3)
+
+#Plot gold prices data
+plot(gold_prices)  
+
